c81d27fdd3550d3c7097a379d96958b29e9533c7
jason030 <jason@uzen.io>
2022-06-23 23:16:30 +0900

    section16-
    from office

--- src/main/java/com/springboot/blog2/SpringbootBlog2RestApiApplication.java
+++ src/main/java/com/springboot/blog2/SpringbootBlog2RestApiApplication.java
@@ -1,12 +1,16 @@
 package com.springboot.blog2;
 
+import com.springboot.blog2.entity.Role;
+import com.springboot.blog2.repository.RoleRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SpringbootBlog2RestApiApplication {
+public class SpringbootBlog2RestApiApplication implements CommandLineRunner {
 
 	@Bean
 	public ModelMapper modelMapper() {
@@ -17,4 +21,18 @@ public class SpringbootBlog2RestApiApplication {
 		SpringApplication.run(SpringbootBlog2RestApiApplication.class, args);
 	}
 
+
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+
+		Role userRole = new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
+	}
 }
